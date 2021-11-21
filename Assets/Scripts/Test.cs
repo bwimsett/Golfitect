0f639f0221f133a5d3f198eef9abd6294b86d966
Ben <b.wimsett@yahoo.co.uk>
2021-11-21 15:28:13 +0000

    Made test changes

--- Assets/Scripts/Test.cs
+++ Assets/Scripts/Test.cs
@@ -7,12 +7,7 @@ public class Test : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        Debug.Log("Hello world");
     }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+    
 }
