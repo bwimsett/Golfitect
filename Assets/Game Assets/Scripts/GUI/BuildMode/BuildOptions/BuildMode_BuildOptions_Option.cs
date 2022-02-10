5748f6743d4cf97bc302d6686072d737d5ea0417
Ben <b.wimsett@yahoo.co.uk>
2022-02-10 18:15:26 +0000

    Added item preview generation for build menu

--- Assets/Game Assets/Scripts/GUI/BuildMode/BuildOptions/BuildMode_BuildOptions_Option.cs
+++ Assets/Game Assets/Scripts/GUI/BuildMode/BuildOptions/BuildMode_BuildOptions_Option.cs
@@ -1,13 +1,22 @@
 using Backend.Level;
 using UnityEngine;
+using UnityEngine.UI;
 
 namespace Game_Assets.Scripts.GUI.LevelBuilder.BuildOptions {
 	public class BuildMode_BuildOptions_Option : MonoBehaviour {
 
 		private LevelObject levelObject;
+		[SerializeField] private Image icon;
 		
 		public void SetLevelObject(LevelObject levelObject) {
 			this.levelObject = levelObject;
+			RuntimePreviewGenerator.BackgroundColor = Color.clear;
+			RuntimePreviewGenerator.OrthographicMode = true;
+			RuntimePreviewGenerator.Padding = 0;
+			Rect rect = new Rect(0,0, icon.rectTransform.rect.width, icon.rectTransform.rect.height);
+			RuntimePreviewGenerator.GenerateModelPreviewAsync(texture => {
+				icon.sprite = Sprite.Create(texture, rect, Vector2.zero);
+			}, levelObject.transform, Mathf.RoundToInt(rect.width), Mathf.RoundToInt(rect.height), true);
 		}
 
 		public void OnClick() {
